63a6abd6dd50db5ad4e2f7b8460fc96642b93e08
guyasong7 <83235576+guyasong7@users.noreply.github.com>
2021-05-21 18:19:55 +0100

    Add files via upload

---
+++ Project_Tip-Calculator-master/Project Tip Cal/script.js
@@ -0,0 +1,34 @@
+
+
+function calculateTip() {
+    var billAmount = document.getElementById("billAmount").value;
+    var serviceQuality = document.getElementById("serviceQuality").value;
+    var numPeople = document.getElementById("totalPeople").value;
+
+    if(billAmount === "" || serviceQuality == 0) {
+        window.alert("Please enter some values to get this baby up and running!");
+        return;
+    }
+
+    if(numPeople === "" || numPeople <= 1) {
+        numPeople = 1;
+
+        document.getElementById("each").style.display = "none";
+    } else {
+        document.getElementById("each").style.display = "block";
+    }
+
+    //Do Math
+    var total = (billAmount * serviceQuality) / numPeople;
+    total = Math.round(total * 100) / 100;
+    
+
+    //Display Tip
+    document.getElementById("totalTip").style.display = "block";
+    document.getElementById("tip").innerHTML = total;
+}
+
+document.getElementById("totalTip").style.display = "none";
+document.getElementById("each").style.display = "none";
+
+document.getElementById("calculate").onclick = function() { calculateTip(); };
---
+++ Project_Tip-Calculator-master/README.md
@@ -0,0 +1,2 @@
+# Project_Tip-Calculator
+A tip calculator made with html, css and javascript
